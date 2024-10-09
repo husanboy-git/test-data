dc9898a5b6883c3def97ff4fbd5488a13b33347d
husanboy <hus19950319@gmail.com>
2024-10-09 20:15:35 +0900

    Initial commit

---
+++ src/test/java/uno/fastcompus/testdata/TestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package uno.fastcompus.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
